0ff8398ad29098d8976697138cf3c99082dda88d
htucker77 <htucker77@github.com>
2017-12-06 13:01:55 -0600

    added files
    wow

---
+++ davitospinner/scripts.js
@@ -0,0 +1,13 @@
+/* global $ */
+var Propeller
+    var maxSpeed = 0;
+
+    var spinner = document.getElementsByClassName('spinner')[0];
+    new Propeller(spinner, {
+        inertia: 0.99,
+    });
+
+
+
+
+
